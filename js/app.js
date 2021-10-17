ec44735562dfeaf847f287789cfb7951a5c5382c
karinabolat <90366213+karinabolat@users.noreply.github.com>
2021-10-17 01:59:19 -0500

    Added comments and addressed issue with keyboard input
    To prevent repeated input of one letter from physical keyboard

--- js/app.js
+++ js/app.js
@@ -5,23 +5,34 @@
 let game;
 const button = document.getElementById('btn__reset');
 
+/**
+ * Listens for click on `#btn_reset` and calls startGame() on game object.
+ */
 button.addEventListener('click', e => {
-    game = new Game;
+    game = new Game();
     game.startGame();
 })
 
 const keyboard = document.getElementById('qwerty');
 
+/**
+ * Listens for user input from onscreen keyboard.
+ */
 keyboard.addEventListener('click', e => {
     if (e.target.tagName === 'BUTTON') {
         game.handleInteraction(e.target.textContent);
     }
 });
 
+/**
+ * Listens for user input from physical keyboard.
+ */
 document.addEventListener('keyup', e => {
     const regex = /^[a-z]$/i;
     if (regex.test(e.key)) {
         const letter = e.key.toLowerCase();
-        game.handleInteraction(letter);
+        if (!game.isLetterRepeated(letter)) {
+            game.handleInteraction(letter);
+        }
     }
 });
